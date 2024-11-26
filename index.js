f54d9bb31e71c75aa4165759b1e85675bf2c9100
Kris <kristinaovchinnikova22@gmail.com>
2024-11-26 20:52:51 +0600

    did a password decryption

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ app.get('/encode/:text', (req, res) => {
     return res.send(encryptedText);
 });
 
-
+app.get('/decode/:text', (req, res) => {
+    const { text } = req.params;
+    const cipher =  Vigenere.Decipher(password);
+    const decryptedText = cipher.crypt(text);
+    return res.send(decryptedText);
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
